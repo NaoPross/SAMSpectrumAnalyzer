8a4e83a3047a97e04a94b0bdc076498d829a1adb
Nao Pross <naopross@thearcway.org>
2018-04-12 16:31:45 +0200

    Add RS232 library from Xylophone project

---
+++ src/rs232.c
@@ -0,0 +1,84 @@
+#include "rs232.h"
+#include <xc.h>
+
+void eusart1_init(void)
+{
+    // set Async and 8 bits frame
+    TXSTA1bits.SYNC = 0;
+    TXSTA1bits.TX9 = 0;
+    
+    // baud prescaler
+    RCSTA1bits.SPEN = 1;
+    SPBRG1 = 103;       // 9600 bps
+    SPBRGH1 = 0;
+    TXSTA1bits.BRGH = 0;
+    BAUDCON1bits.BRG16 = 0;
+    
+    // set up TX / RX pins
+    TRISCbits.TRISC7 = 1;
+    TRISCbits.TRISC6 = 1;
+    // enable continuous reception
+    RCSTA1bits.CREN = 1;
+    TXSTA1bits.TXEN = 1;
+}
+
+void eusart2_init(void)
+{
+    // set Async and 8 bits frame
+    TXSTA2bits.SYNC = 0;
+    TXSTA2bits.TX9 = 0;
+    
+    // baud prescaler
+    RCSTA2bits.SPEN = 1;
+    SPBRG2 = 103;			// 9600 bps
+    SPBRGH2 = 0;
+    TXSTA2bits.BRGH = 0;
+    BAUDCON2bits.BRG16 = 0;
+    
+    // set up TX / RX pins
+    TRISDbits.TRISD7 = 1;
+    TRISDbits.TRISD6 = 1;
+    // enable continuous reception
+    RCSTA2bits.CREN = 1;
+    TXSTA2bits.TXEN = 1;
+}
+
+void eusart1_putch(char c)
+{
+    while (!TX1IF);
+    TX1REG = c;
+}
+
+void eusart2_putch(char c)
+{
+    while (!TX2IF);
+    TX2REG = c;
+}
+
+char eusart1_getch(void)
+{
+    while (!RC1IF);
+    return RC1REG;
+}
+
+char eusart2_getch(void)
+{
+    while (!RC2IF);
+    return RC2REG;
+}
+
+char eusart1_getche(void)
+{
+    char c = eusart1_getch();
+    eusart1_putch(c); // echo
+    
+    return c;
+}
+
+char eusart2_getche(void)
+{
+    char c = eusart2_getch();
+    eusart2_putch(c); // echo
+    
+    return c;
+}
---
+++ src/rs232.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   rs232.h
+ * Author: _prossn
+ *
+ * Created on 7. dicembre 2017, 15:07
+ */
+
+#ifndef RS232_H
+#define	RS232_H
+
+
+extern void eusart1_init(void);
+extern void eusart2_init(void);
+
+extern void eusart1_putch(char c);
+extern void eusart2_putch(char c);
+
+extern char eusart1_getch(void);
+extern char eusart2_getch(void);
+
+extern char eusart1_getche(void);
+extern char eusart2_getche(void);
+
+#endif	/* RS232_H */
+
