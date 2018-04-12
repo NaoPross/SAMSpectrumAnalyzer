eb5f315e3cf20a5f922a71aea9b47dfaffdf8eed
Nao Pross <naopross@thearcway.org>
2018-04-12 12:02:48 +0200

    Add empty PIC project for uC development

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * File:    main.c
+ * Author:  Naoki Pross
+ * Project: SAM Spectrum Analyzer
+ * 
+ * Created on 12.04.2018, 11:57
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
