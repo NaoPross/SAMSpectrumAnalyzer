69d5d425552a9eecee7f168bfa8912061e4dd876
Nao Pross <naopross@thearcway.org>
2018-04-26 16:07:29 +0200

    Implement basic plotting

--- src-desktop/serialworker.cpp
+++ src-desktop/serialworker.cpp
@@ -18,15 +18,16 @@ void SerialWorker::run()
 {
     while (!isInterruptionRequested()) {
         QMutexLocker locker(&_mutex);
-        while (!_serial.available() && !isInterruptionRequested());
+        while ((_serial.available() <= 8)&& !isInterruptionRequested());
 
-        for (std::string line : _serial.readlines()) {
-            QString data = QString::fromStdString(line);
-            emit receivedData(data);
-        }
+        // for (std::string line : _serial.readlines(65536, "\n\r")) {
+        //     QString data = QString::fromStdString(line);
+        //     emit receivedData(data);
+        // }
 
-        // QString data = QString::fromStdString(_serial.readline());
-        // emit receivedData(data);
-        _serial.flushOutput();
+        _serial.waitReadable();
+        QString data = QString::fromStdString(_serial.readline(65536, "\n\r"));
+        emit receivedData(data);
+        // _serial.flushOutput();
     }
 }
