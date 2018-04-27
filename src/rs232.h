0d003a09e428067c93a9a90e032bf4f3eb8ccce2
Nao Pross <naopross@thearcway.org>
2018-04-27 16:29:15 +0200

    Add arbitrary data function to RS232 library to make the main more readable

--- src/rs232.h
+++ src/rs232.h
@@ -8,6 +8,8 @@
 #ifndef RS232_H
 #define	RS232_H
 
+#include <stddef.h>
+
 // defines which eusart device is used with prinf()
 #define EUSART_1_PRINTF 1
 
@@ -22,6 +24,9 @@ extern char eusart2_getch(void);
 
 extern char eusart1_getche(void);
 extern char eusart2_getche(void);
+
+extern void eusart1_write(void *data, size_t len);
+
 
 #endif	/* RS232_H */
 
