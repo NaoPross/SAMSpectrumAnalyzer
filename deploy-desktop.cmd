b497c5aad86b5798b4d2cec64849a61d471b1e87
Nao Pross <naopross@thearcway.org>
2018-05-14 14:12:18 +0200

    Update windows deploy script

--- deploy-desktop.cmd
+++ deploy-desktop.cmd
@@ -1,5 +1,15 @@
-set QT_PATH=C:\OtherPrograms\Qt
-set QT_VERSION=5.10.0
+set QT_PATH=C:\Qt
+set QT_VERSION=5.10.1
+set MINGW_VERSION=mingw53_32
 
-%QT_PATH%\%QT_VERSION%\mingw53_32\bin\windeployqt.exe --release build-deskop\release\SpectrumAnalyzer.exe
+set QT_MINGW_PATH=%QT_PATH%\%QT_VERSION%\%MINGW_VERSION%\bin
+set QT_TOOL_MINGW_PATH=%QT_PATH%\Tools\mingw530_32\bin
+
+:: Example path for MSVC 32-bit
+:: PATH=%QT_PATH%\%QT_VERSION%\msvc2013\bin;%PATH%
+
+:: Path for MinGW g++
+PATH=%QT_MINGW_PATH%;%QT_TOOL_MINGW_PATH%;%PATH% 
+
+windeployqt.exe --release --force %~dp0\build-desktop\release\SpectrumAnalyzer.exe
 pause
